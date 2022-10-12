952e3541872e74e787c7250b4bc6ba812198388f
qsaval <quentin.saval@hotmail.fr>
2022-10-12 10:54:08 +0200

    modif_de_exercice

--- exercice_javascript/page.js
+++ exercice_javascript/page.js
@@ -1,2 +1,9 @@
 var reponse1 = window.prompt("saisisser votre nom")
-var reponse1 = window.prompt("saisisser votre prenom")
+var reponse2 = window.prompt("saisisser votre prenom")
+if(window.confirm("Etes-vous un homme?") == true){
+    window.alert("Bonjour Monsieur" + reponse1 + reponse2 + "bienvenue sur notre site web!") 
+}
+else{
+    window.alert("Bonjour Madame" + reponse1 + reponse2 + "bienvenue sur notre site web!")
+}
+

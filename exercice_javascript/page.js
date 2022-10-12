5c038f633997d572360ae8c5f3786c6e17da648d
qsaval <quentin.saval@hotmail.fr>
2022-10-12 10:55:54 +0200

    modif_de_exercice

--- exercice_javascript/page.js
+++ exercice_javascript/page.js
@@ -1,9 +1,9 @@
-var reponse1 = window.prompt("saisisser votre nom")
-var reponse2 = window.prompt("saisisser votre prenom")
+var reponse1 = window.prompt("saisisser votre nom");
+var reponse2 = window.prompt("saisisser votre prenom");
 if(window.confirm("Etes-vous un homme?") == true){
-    window.alert("Bonjour Monsieur" + reponse1 + reponse2 + "bienvenue sur notre site web!") 
+    window.alert("Bonjour Monsieur" + reponse1 + reponse2 + "bienvenue sur notre site web!"); 
 }
 else{
-    window.alert("Bonjour Madame" + reponse1 + reponse2 + "bienvenue sur notre site web!")
+    window.alert("Bonjour Madame" + reponse1 + reponse2 + "bienvenue sur notre site web!");
 }
 

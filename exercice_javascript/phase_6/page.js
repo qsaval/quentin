d6fb1a1ada33d56e38354e49773eb8c95fb19d85
qsaval <quentin.saval@hotmail.fr>
2022-10-12 11:37:05 +0200

    exercice_terminer

---
+++ exercice_javascript/phase_6/page.js
@@ -0,0 +1,11 @@
+var a = "100";
+var b = 100;
+var c = 1.00;
+var d = true;
+document.write("Ceci est une chaine de caractères : " + a);
+document.write("Ceci est une chaine de caractères : 100");
+b--;
+c = a;
+if (d == true){
+    d = false;
+}

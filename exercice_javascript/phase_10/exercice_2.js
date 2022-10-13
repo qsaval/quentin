5e0a828c1e2a2230a68712849c906e598fc41751
qsaval <quentin.saval@hotmail.fr>
2022-10-13 15:13:48 +0200

    modif

--- exercice_javascript/phase_10/exercice_2.js
+++ exercice_javascript/phase_10/exercice_2.js
@@ -3,20 +3,24 @@ function GetInteger(){
     a=window.prompt("saisir un entier");
     return a;
 }
+
 function InitTab(){
     var a;
     a=window.prompt("taille du tableau");
     return a;
 }
+
 function SaisirTab(tab,n){
     for(var i=0;i<=n-1;i++){
-        tab[i]=window.prompt();
+        tab[i]=window.prompt("saisir les valeur du tableau");
     }
     return tab;
 }
+
 function AffichTab(tab){
     document.write(tab);
 }
+
 function RechercheTab(tab,n){
     var a;
     a=tab.length;
@@ -26,41 +30,41 @@ function RechercheTab(tab,n){
         }
     }
 }
+
 function InfoTab(tab){
     var a;
-    var b;
-    var c;
-    var d=0;
+    var c=0;
+    var d;
     var e;
-    var g;
+    var g=[];
     a=tab.length;
     for(var i=0;i<=a-2;i++){
-        if(b[i]<b[i+1]){
-            c=b[i+1]
+        if(tab[i]<tab[i+1]){
+            c=tab[i+1];
         }
         else{
-            c=b[i]
+            c=tab[i];
         }
     }
     for(var f=0;f<=a-1;f++){
-        d=d+b[i]
+        d=d+tab[f];
     }
-    e=d/a
-    g=[c,e]
+    e=d/a;
+    g=[c,e];
     return g;
 }
+
+
 var a= GetInteger();
 var b=InitTab();
 var c=[];
 var d;
-var e;
-var f;
-var g;
+var e=[];
+
 c=SaisirTab(c,b);
 d=RechercheTab(c,a);
 AffichTab(c);
-document.write(d);
+document.write("<br>la valeur au rang "+a+" est "+d);
 e=InfoTab(c);
-f=e[0];
-g=e[1];
-document.write("le maximum est "+ f +" la moyenne est "+g)
+console.log(e);
+document.write("<br>le maximum est "+e[0]+ "<br>la moyenne est "+e[1]);
--- exercice_javascript/phase_10/exercice_3.js
+++ exercice_javascript/phase_10/exercice_3.js
@@ -1,5 +1,6 @@
 var b=[1,5,3,1,4,2,5];
 var a= b.length;
+var c;
 for (var i=0;i<=a;i++){
     if(b[i]>b[i+1]){
         c=b[i];

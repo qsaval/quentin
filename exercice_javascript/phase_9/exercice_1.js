4e52e389008ea5d65fd6b9b538dbcb99aa5c944c
qsaval <quentin.saval@hotmail.fr>
2022-10-13 10:48:56 +0200

    exercice_1_modif

--- exercice_javascript/phase_9/exercice_1.js
+++ exercice_javascript/phase_9/exercice_1.js
@@ -4,10 +4,11 @@ function produit (x,y){
     return a;
 }
 function afficheImg(image){
-    prompt(image);
+    console.log (image);
 }
 var a= window.prompt("choisir un nombre");
 var b= window.prompt("choisir un nombre");
 var c;
 c= produit(a,b);
+console.log("le produit de " + a + "par" + b + "est egal a" + c)
 

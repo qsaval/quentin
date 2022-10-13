712980bf46838137f348e04f50ad0ee9d0414505
qsaval <quentin.saval@hotmail.fr>
2022-10-13 16:35:41 +0200

    modif

--- exercice_javascript/phase_10/exercice_3.js
+++ exercice_javascript/phase_10/exercice_3.js
@@ -1,4 +1,4 @@
-var b=[1,5,3,1,4,2,5];
+function tri(b){
 var a= b.length;
 var c;
 for (var i=0;i<=a;i++){
@@ -8,3 +8,12 @@ for (var i=0;i<=a;i++){
         b[i+1]=c;
     }
 }
+return b;
+}
+var a;
+var b=[1,6,4,2,8,1];
+a=b.length;
+for(var i=0;i<=a;i++){
+    b=tri(b);
+}
+document.write(b);

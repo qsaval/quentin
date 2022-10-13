d4655aee3529e7f80e2047a2ca47e83ed546b3a9
qsaval <quentin.saval@hotmail.fr>
2022-10-13 10:28:56 +0200

    exercice_1

---
+++ exercice_javascript/phase_9/exercice_1.js
@@ -0,0 +1,13 @@
+function produit (x,y){
+    var a;
+    a=x*y;
+    return a;
+}
+function afficheImg(image){
+    prompt(image);
+}
+var a= window.prompt("choisir un nombre");
+var b= window.prompt("choisir un nombre");
+var c;
+c= produit(a,b);
+

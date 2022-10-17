507e6d38ad778abc45640c46191da80465ee64a3
qsaval <quentin.saval@hotmail.fr>
2022-10-17 09:26:23 +0200

    exercice_terminer

---
+++ exercice_javascript/phase_13/exercice.js
@@ -0,0 +1,32 @@
+function somme (tab){
+    var a= tab.length;
+    var b=0;
+
+    for (var i=0;i<=a-1;i++){
+        b=b+parseInt(tab[i]);
+    }
+    return b;
+}
+
+function moyenne (tab){
+    var a= tab.length;
+    var b;
+    var c;
+
+    b=somme(tab);
+    c=b/a;
+    return c;
+}
+
+
+var a =[];
+var i=0;
+
+a[i]=window.prompt();
+while (a[i]!=0){
+    i++;
+    a[i]=window.prompt();
+}
+b=somme(a);
+c=moyenne(a); 
+document.write(a+"<br> la somme est "+b+ "<br> la moyenne est "+c);

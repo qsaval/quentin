f7cb244b230bffdf164ea8689d61a59de0ccb878
qsaval <quentin.saval@hotmail.fr>
2022-10-17 08:44:45 +0200

    modif_exercice_2

--- exercice_javascript/phase_7/exercice_2.js
+++ exercice_javascript/phase_7/exercice_2.js
@@ -1,6 +1,9 @@
-var a= window.prompt();
+var a= window.prompt("quelle est ton annee de naissance");
 var b;
-b=2022/a;
+var c= Date();
+var d=c.getFullYear();
+document.write(d);
+b=c/a;
 if(b>=18){
     document.write("ton age est " + b + " et tu est majeur");
 }

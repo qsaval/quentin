b6a64a39153e1c770b261657d784bd284a9d4fd3
qsaval <quentin.saval@hotmail.fr>
2022-10-18 15:41:12 +0200

    modif.exercice

--- exercice_javascript/phase_16/exercice.js
+++ exercice_javascript/phase_16/exercice.js
@@ -7,6 +7,8 @@ document.getElementById("contact").addEventListener("submit",function (e){
     var ville=document.getElementById("ville");
     var code=document.getElementById("code");
     var email=document.getElementById("email");
+    var a=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var b;
 
     if(societe.value==""){
         erreur="entrez la nom de société s.v.p";
@@ -17,11 +19,12 @@ document.getElementById("contact").addEventListener("submit",function (e){
     if(ville.value==""){
         erreur="entrez le nom de la ville de l'entreprise s.v.p";
     }
-    
     if(code.value.length<5&& code.value.length>0){
-        erreur="entrez le code postal sur 5 chiffre s.v.p";
+        er
+        reur="entrez le code postal sur 5 chiffre s.v.p";
     }
-    if(email){
+    b=a.test(email);
+    if(b){
         erreur="entrez email de entreprise societe s.v.p";
     }
     if(erreur){

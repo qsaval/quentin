aa5942c220b3382c7e7d4d9ec297a1d894d464ec
qsaval <quentin.saval@hotmail.fr>
2022-10-19 11:40:21 +0200

    exercice_2_terminer

---
+++ evaluation_javascript/exercice_2.js
@@ -0,0 +1,13 @@
+function TableMultiplication(m){
+    var a;
+
+    document.write("table de "+m+" :<br><br>");
+    for(var i=1;i<=10;i++){
+        a=i*m;
+        document.write(i+" x "+m+" = "+a+"<br>");
+    }
+}
+
+var a=window.prompt("choisir la table");
+
+TableMultiplication(a);

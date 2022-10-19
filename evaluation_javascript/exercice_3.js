b0b78d2c104a7f582d7505c77dad2fa88b0031f7
qsaval <quentin.saval@hotmail.fr>
2022-10-19 13:20:48 +0200

    exercice_3

--- evaluation_javascript/exercice_3.js
+++ evaluation_javascript/exercice_3.js
@@ -1,2 +1,14 @@
 var tab=["Audrey","Aurélien","Flavien","Jérémy","Laurent","Malik","Nouara","Salem","Samuel","Stéphane"];
 var prenom = window.prompt("entre un prenom");
+var a=tab.length;
+var b;
+for (var i=0;i<=a-1;i++){
+    if(prenom==tab[i]){
+        prenom="";
+        for(var f=i;f<=a-2;i++){
+            b[f]=b[f+1];
+        }
+        b[a-1]="";
+    }
+}
+document.write(b);

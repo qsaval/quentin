acf90817c0f959341a08491568a4f2e3cf27ab6a
qsaval <quentin.saval@hotmail.fr>
2022-10-20 09:38:39 +0200

    modif_exercice

--- evaluation_javascript/exercice_3.js
+++ evaluation_javascript/exercice_3.js
@@ -1,14 +1,15 @@
-var tab=["Audrey","Aurélien","Flavien","Jérémy","Laurent","Malik","Nouara","Salem","Samuel","Stéphane"];
+var tab=["Audrey","Aurélien","Flavien","Jérémy","Laurent","Melik","Nouara","Salem","Samuel","Stéphane"];
 var prenom = window.prompt("entre un prenom");
 var a=tab.length;
+var b;
 
 for (var i=0;i<=a-1;i++){
     if(prenom==tab[i]){
         tab[i]="";
-        for(var f=i;f<=a-2;i++){
-            tab[f]=tab[f+1];
+        b=i;
+        for(var f=b;f<=a-2;i++){
+            tab[f]=tab[f+1].value;
         }
-        tab[a-1]="";
     }
 }
 if(tab[a-1]!=""){

91b30dcd23b1edbe6e06e0cfee05afadc0e71398
qsaval <quentin.saval@hotmail.fr>
2022-10-21 10:05:45 +0200

    modif

--- fil_rouge_jarditou/assets/javascript/page.js
+++ fil_rouge_jarditou/assets/javascript/page.js
@@ -7,7 +7,6 @@ document.getElementById("contact").addEventListener("submit",function (e){
     var code=document.getElementById("code");
     var email=document.getElementById("email");
     var a=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    var b;
     var c=/^[A-Z,a-z]+$/;
     var d=/^[0-9]+$/;
     
@@ -20,8 +19,7 @@ document.getElementById("contact").addEventListener("submit",function (e){
     if(code.value.length<5&& code.value.length>0){
         erreur="entrez le code postal sur 5 chiffre s.v.p";
     }
-    b=a.test(email);
-    if(b){
+    if(a.test(email)){
         erreur="entrez email de entreprise societe s.v.p";
     }
     if(!c.test(nom.value)){

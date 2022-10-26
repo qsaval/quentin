c2c1fc964518ead82b9f22b3ea20ab03081d2030
qsaval <quentin.saval@hotmail.fr>
2022-10-26 08:49:49 +0200

    modif_evaluation

--- evaluation_javascript/exercice_3.js
+++ evaluation_javascript/exercice_3.js
@@ -2,16 +2,26 @@ document.getElementById("exercice3").addEventListener("submit",function (){
 var tab=["Audrey","Aurélien","Flavien","Jérémy","Laurent","Melik","Nouara","Salem","Samuel","Stéphane"];
 var prenom = window.prompt("entre un prenom");
 var a=tab.length;
-
+var b;
+var c;
+var d;
+var e;
 
 for (var i=0;i<=a-1;i++){
-    if(prenom==tab[i]){;
-        for(var f=i;f<=a-2;i++){
-            tab[f]=tab[f+1].valueOf;
-        }
+    c=tab[i];
+    if(prenom==c){;
+        tab[i]="";
+        b=i;
     }
 }
-tab[a-1]="";
+if(tab[b]==""){
+    for(var f=b;f<=a-2;i++){
+        d=f+1;
+        e=tab[d];
+        tab[f]=e;
+        tab[a-1]="";
+    }
+}
 if(tab[a-1]!=""){
     alert("le prenom n'est pas dans la liste");
 }

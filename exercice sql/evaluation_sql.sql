ece56b325b3c02cf6be236efeb68bc3fb0fa8635
qsaval <quentin.saval@hotmail.fr>
2022-12-12 10:25:16 +0100

    modif

--- exercice sql/evaluation_sql.sql
+++ exercice sql/evaluation_sql.sql
@@ -4,9 +4,33 @@ from customers c
 where Country = 'France';
 
 --2--
+SELECT ProductName, UnitPrice
+from products p 
+join suppliers s on suppliers.SupplierID = products.SupplierID 
+WHERE CompanyName = 'Exotic Liquide';
+
+--3--
+SELECT CompanyName, ShipVia
+from customers c
+join orders o on orders.CustomerID = customers.CustomerID
+WHERE Country = 'France';
+
+--4--
 
 
+--5--
+
+
+--6--
 
 --7--
 SELECT SUM(UnitPrice * Quantity)
 from `order details`
+
+--8--
+
+
+--9--
+
+
+--10--

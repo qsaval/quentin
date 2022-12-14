02b14662dded60d67631702ce1d6225a645e147c
qsaval <quentin.saval@hotmail.fr>
2022-12-14 13:41:33 +0100

    modif_php

--- exercice_php/exercice_boucle_3.php
+++ exercice_php/exercice_boucle_3.php
@@ -27,124 +27,18 @@
                             }
                         ?>
                     </tr>
-                    <tr> 
-                        <?php
-                            echo "<th> 1 </th>"; 
+                    <?php
+                        $b;
+                        for($c=1; $c<=12; $c++){
+                            echo "<tr>"; 
+                            echo "<th>".$c."</th>"; 
                             for($a=0; $a<13; $a++){
-                                echo "<td>".$a."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 2 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*2;
+                                $b=$c*$a;
                                 echo "<td>".$b."</td>";
                             }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 3 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*3;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 4 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*4;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 5 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*5;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 6 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*6;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 7 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*7;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 8 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*8;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 9 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*9;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 10 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*10;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 11 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*11;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            $b;
-                            echo "<th> 12 </th>";
-                            for($a=0; $a<=12; $a++){
-                                $b=$a*12;
-                                echo "<td>".$b."</td>";
-                            }
-                        ?>
-                    </tr>
+                            echo "</tr>";
+                        }
+                    ?>
                 </tbody>
             </table>
         </body>

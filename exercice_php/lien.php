6016004ee8f8a00f5b324ec91f08b9a88494182e
qsaval <quentin.saval@hotmail.fr>
2022-12-19 16:40:16 +0100

    modif

---
+++ exercice_php/lien.php
@@ -0,0 +1,8 @@
+<?php
+    $l = file("lien.txt");
+    echo "<a href='".$l[0]."'>".$l[0]."<a><br>";
+    echo "<a href='".$l[1]."'>".$l[1]."<a><br>";
+    echo "<a href='".$l[2]."'>".$l[2]."<a><br>";
+    echo "<a href='".$l[3]."'>".$l[3]."<a><br>";
+    echo "<a href='".$l[4]."'>".$l[4]."<a><br>";
+?> 

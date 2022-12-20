81bcdc53435556e44a3c69e13e86a3e473470c17
qsaval <quentin.saval@hotmail.fr>
2022-12-20 14:27:46 +0100

    rajout

---
+++ les_disques_de_Velvet_Record/disc_new.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PDO - Ajout</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="p-2">        
+        <h1>Le formulaire d'ajout</h1>
+        <h2>Ajouter un vinyle</h2>
+    </div>
+
+    <div class="p-3">
+        <form action ="script_disc_ajout.php" method="post" >
+
+            <label for="titre" class="form-label">Title</label><br>
+            <input type="text" name="titre" placeholder="Enter title" class="form-control">
+            <br><br>
+
+            <label for="nom" class="form-label">Artist</label><br>
+            <input type="text" name="nom" placeholder="Queens of the stone Age" class="form-control">
+            <br><br>
+
+            <label for="annee" class="form-label">Year</label><br>
+            <input type="text" name="annee" placeholder="Enter year" class="form-control">
+            <br><br>
+
+            <label for="genre" class="form-label">Genre</label><br>
+            <input type="text" name="genre" placeholder="Enter genre (Rock, Pop, Prog...)" class="form-control">
+            <br><br>
+
+            <label for="label" class="form-label">Label</label><br>
+            <input type="text" name="label" placeholder="Enter label (EMI, Warner, PolyGram, Universale...)" class="form-control">
+            <br><br>
+
+            <label for="label" class="form-label">price</label><br>
+            <input type="text" name="label" class="form-control">
+            <br><br>
+
+            <label for="image" class="form-label">Picture</label><br>
+            <button><input type="file" name="image"  class="form-control"></button>
+            <br><br>
+
+            <input type="submit" value="Ajouter" class="btn btn-primary">
+            <a href="discs.php"><button type="button" class="btn btn-primary">Retour</button></a>
+        </form>
+    </div>
+</body>
---
+++ les_disques_de_Velvet_Record/discs.php
@@ -0,0 +1,48 @@
+<?php
+    include "db.php";
+    $db = connexionBase();
+
+    $requete = $db->query("SELECT * FROM disc join artist on artist.artist_id = disc.artist_id");
+    $tableau = $requete->fetchAll(PDO::FETCH_OBJ);
+    $requete->closeCursor();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO - Liste</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <header class="d-flex justify-content-between">
+        <h1>Liste des disque(15)</h1>
+        <a href="disc_new.php"><button type="button" class="btn btn-primary">Ajouter</button></a>
+    </header>
+    <div>
+    <div class="d-inline p-2" >    
+    <?php foreach ($tableau as $disc): ?>
+        <div class="d-flex">
+            <div class="d-inline p-2" >
+                <img src="jaquettes/<?= $disc->disc_title ?>.jpeg" alt="">
+            </div>
+            <div>
+                <h2><?= $disc->disc_title ?></h2>
+                <h3><?= $disc->artist_name ?></h3>
+                <p>Label: <?= $disc->disc_label ?></p>
+                <p>Year: <?= $disc->disc_year ?></p>
+                <p>Genre: <?= $disc->disc_genre ?></p>
+
+                <a href="disc_detail.php?id=<?= $artist->artist_id ?>"><button type="button" class="btn btn-primary">Détail</button></a>
+            </div>
+        </div>
+    <?php endforeach; ?>
+    </div>
+    </div>
+</body>
+</html>

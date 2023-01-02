74427d2a48a15e48b9d636c12acb57eb54c64f6d
qsaval <quentin.saval@hotmail.fr>
2023-01-02 11:20:58 +0100

    modif

--- login/login_form.php
+++ login/login_form.php
@@ -5,18 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <h1>connexion</h1>
+    <h1>Connexion</h1>
     <form action="login_script.php" method="post">
-        <label for="login"> Email :</label>
-        <input type="text" name="login"><br>
-        <label for="mdp">Mot de passe :</label>
-        <input type="password" name="mdp"><br>
+        <div class="mb-3">
+            <label for="login" class="form-label"> Email :</label>
+            <input type="text" name="login" class="form-control"><br>
+            <label for="mdp" class="form-label">Mot de passe :</label>
+            <input type="password" name="mdp" class="form-control">
+        </div>
 
-        <input type="submit" value="Valider" >
-        <input type="reset" value="Annuler"><br>
-        <a href="login_form_inscription.php"> inscription </a>
+        <input type="submit" value="Valider" class="btn btn-secondary">
+        <input type="reset" value="Annuler" class="btn btn-secondary"><br><br>
+        <a href="login_form_inscription.php" class="btn btn-secondary"> inscription </a>
     </form>
 </body>
 </html>
--- login/login_form_inscription.php
+++ login/login_form_inscription.php
@@ -5,26 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <h1>inscription</h1>
+    <h1>Inscription</h1>
     <form action="script_inscription.php" method="post">
-        <label for="nom">Nom</label>
-        <input type="text" name="nom"><br>
+        <div class="mb-3">
+            <label for="nom" class="form-label">Nom</label>
+            <input type="text" name="nom" class="form-control"><br>
 
-        <label for="prenom">Prenom</label>
-        <input type="text" name="prenom"><br>
+            <label for="prenom" class="form-label">Prenom</label>
+            <input type="text" name="prenom" class="form-control"><br>
         
-        <label for="login"> Email</label>
-        <input type="text" name="login"><br>
+            <label for="login" class="form-label"> Email</label>
+            <input type="text" name="login" class="form-control"><br>
         
-        <label for="mdp">Mot de passe</label>
-        <input type="password" name="mdp"><br>
+            <label for="mdp" class="form-label">Mot de passe</label>
+            <input type="password" name="mdp"class="form-control">
+        </div>
 
-        <input type="submit" value="Valider" >
-        <input type="reset" value="Annuler">
-        <br>
-        <a href="login_form.php"> retour </a>
+        <input type="submit" value="Valider" class="btn btn-secondary">
+        <input type="reset" value="Annuler" class="btn btn-secondary">
+        <br><br>
+        <a href="login_form.php" class="btn btn-secondary"> retour </a>
     </form>
 </body>
 </html>
